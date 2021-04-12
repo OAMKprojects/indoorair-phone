93d05d65f3a6484b3f932987e3f6dd6a440f2147
Yarviz <juzzzzi@hotmail.com>
2021-04-12 20:47:41 +0300

    update

--- app/src/main/java/indair/basic/AppActivity.java
+++ app/src/main/java/indair/basic/AppActivity.java
@@ -1,38 +1,46 @@
 package indair.basic;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Space;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class AppActivity extends Fragment implements NetworkCallBack {
 
     private Button logOutButton;
-    private TextView textTemperature;
-    private TextView textHumidity;
     private TextView logView;
     private Network  net;
     private MainCallBack main;
     private Crypto crypto;
+    private View view;
+    private Map<String, TextView> value_map;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view =  inflater.inflate(R.layout.activity_app, container, false);
+        view =  inflater.inflate(R.layout.activity_app, container, false);
 
-        textTemperature = view.findViewById(R.id.textViewTemperatureVal);
-        textHumidity = view.findViewById(R.id.textViewHumidityVal);
         logOutButton = view.findViewById(R.id.buttonLogout);
         logView = view.findViewById(R.id.logView);
+        value_map = new HashMap<String, TextView>();
 
         logOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -105,11 +113,15 @@ public class AppActivity extends Fragment implements NetworkCallBack {
         try {
             JSONObject json = new JSONObject(json_message);
 
-            if (json.has("temperature")) {
-                textTemperature.setText(json.getString("temperature"));
-            }
-            if (json.has("humidity")) {
-                textHumidity.setText(json.getString("humidity"));
+            if (json.has("values")) {
+
+                JSONObject values = json.getJSONObject("values");
+                Iterator<String> name_it = values.keys();
+
+                while(name_it.hasNext()) {
+                    String name = name_it.next();
+                    updateOldRow(name, values.getString(name));
+                }
             }
         } catch (JSONException e) {
             logView.setText(json_message);
@@ -117,6 +129,46 @@ public class AppActivity extends Fragment implements NetworkCallBack {
         }
 
         if (logView.length() > 0) logView.setText("");
+    }
+
+    private void createNewRow(String name, String value)
+    {
+        TableRow new_row = new TableRow(getContext());
+        new_row.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        new_row.setGravity(Gravity.CENTER);
+
+        TextView new_name = new TextView(getContext());
+        new_name.setText(name);
+        new_name.setTextSize(24);
+        new_name.setTextColor(Color.BLACK);
+        new_name.setGravity(Gravity.LEFT);
+
+        Space new_space = new Space(getContext());
+        new_space.setLayoutParams(new TableRow.LayoutParams(60, TableRow.LayoutParams.MATCH_PARENT));
+
+        TextView new_value = new TextView(getContext());
+        new_value.setText(value);
+        new_value.setTextSize(24);
+        new_value.setTextColor(Color.BLUE);
+        new_value.setGravity(Gravity.RIGHT);
+
+        new_row.addView(new_name);
+        new_row.addView(new_space);
+        new_row.addView(new_value);
+
+        TableLayout tb = view.findViewById(R.id.valueTable);
+        tb.addView(new_row);
+
+        value_map.put(name, new_value);
+    }
+
+    private void updateOldRow(String name, String value)
+    {
+        if (value_map.containsKey(name)) {
+            TextView old_value = value_map.get(name);
+            old_value.setText(value);
+        }
+        else createNewRow(name, value);
     }
 
     public void startListenig()
